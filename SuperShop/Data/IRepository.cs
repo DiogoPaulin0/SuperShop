4ee31acf0c5930e0e03ec97f9408979ab388b8a3
DiogoPaulin0 <diogopaulino1808@gmail.com>
2024-07-22 09:58:29 +0100

    Generic repository

--- SuperShop/Data/IRepository.cs
+++ SuperShop/Data/IRepository.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using SuperShop.Data.Entities;
+﻿//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using SuperShop.Data.Entities;
 
-namespace SuperShop.Data
-{
-    public interface IRepository
-    {
-        void AddProduct(Product product);
-        Product GetProduct(int id);
-        IEnumerable<Product> GetProducts();
-        bool ProductExists(int id);
-        void RemoveProduct(Product product);
-        Task<bool> SaveAllAsync();
-        void UpdateProduct(Product product);
-    }
-}
+//namespace SuperShop.Data
+//{
+//    public interface IRepository
+//    {
+//        void AddProduct(Product product);
+//        Product GetProduct(int id);
+//        IEnumerable<Product> GetProducts();
+//        bool ProductExists(int id);
+//        void RemoveProduct(Product product);
+//        Task<bool> SaveAllAsync();
+//        void UpdateProduct(Product product);
+//    }
+//}
