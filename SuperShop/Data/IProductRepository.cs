60ab9aa0e4b238a44846186d178f219f63160a58
DiogoPaulin0 <diogopaulino1808@gmail.com>
2024-08-13 21:31:17 +0100

    Products combo

--- SuperShop/Data/IProductRepository.cs
+++ SuperShop/Data/IProductRepository.cs
@@ -1,10 +1,14 @@
-﻿using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using SuperShop.Data.Entities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SuperShop.Data
 {
     public interface IProductRepository : IGenericRepository<Product>
     {
         public IQueryable GetAllWithUsers();
+
+        IEnumerable<SelectListItem> GetComboProducts();
     }
 }
